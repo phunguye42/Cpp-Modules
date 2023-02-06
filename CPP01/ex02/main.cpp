9a365305519faf22d7642b90f925aefc27a7a883
phunguye42 <nlgphuc23@gmail.com>
2023-02-06 17:02:25 +1030

    CPP01 ex03 complete

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(void){
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string & stringREF = string;
+
+    std::cout << &string << ": " << string << std::endl;
+    std::cout << stringPTR << ": " << *stringPTR << std::endl;
+    std::cout << &stringREF << ": " << stringREF << std::endl;
+
+    return 0;
+}
