682a4e2b84955ad44986cdae06e7901008a83d15
phunguye42 <nlgphuc23@gmail.com>
2023-02-16 15:04:36 +1100

    merged

--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -13,16 +13,3 @@ public:
 
 	float toFloat(void) const;
 	int toInt(void) const;
-
-
-=======
-#ifndef Fixed_HPP
-#define Fixed_HPP
-
-#include <iostream>
-
-class Fixed
-{
-	Fixed();
->>>>>>> 6b026e80c084fb9ba8c7a7d18c88416a481f3822
-}
